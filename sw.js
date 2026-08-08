c7abf9bc69532bf3f8dbdc7564db3e38be000768
paes2005-design <paes2005@gmail.com>
2026-08-07 23:40:31 -0300

    Cache shared tolerance timer and review feedback

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'rotina-family-cliente-v6';
-const APP_SHELL = ['./','./index.html','./index-CLIENTE-v6.html','./manifest.json','./icon-cliente-192.png','./icon-cliente-512.png','./client-ui-pro.css','./client-ui-pro.js'];
+const CACHE_NAME = 'rotina-family-cliente-v7';
+const APP_SHELL = ['./','./index.html','./index-CLIENTE-v6.html','./manifest.json','./icon-cliente-192.png','./icon-cliente-512.png','./client-ui-pro.css','./client-ui-pro.js','./client-rules-v2.js'];
 const MODULE_ROOTS = [
   'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js',
   'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js'
